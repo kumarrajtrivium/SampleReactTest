8779a74f7a0c1f87a35e3c4461a98ae709341f47
Kumar Raj Ranjan <Kumar.Raj@trivium-esolutions.com>
2023-05-25 13:43:51 +0530

    format the application using Prettier extension

--- sample-project/src/Component/Message.tsx
+++ sample-project/src/Component/Message.tsx
@@ -1,8 +1,8 @@
-import './message.css';
+import "./message.css";
 
-function Message(){
-    const name = "My First React Application";
-    if(name) return <p className='greenTest'>{name}</p>;
-    return <p  className='greyTest'>Hello world</p>;
+function Message() {
+  const name = "My First React Application";
+  if (name) return <p className="greenTest">{name}</p>;
+  return <p className="greyTest">Hello world</p>;
 }
 export default Message;
